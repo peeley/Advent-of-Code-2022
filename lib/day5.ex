64fabd0220d43858c7507ce93c726a6e3a396543
Noah Snelson <noah.snelson@protonmail.com>
2022-12-20 00:05:07 -0800

    Start day 5

---
+++ lib/day5.ex
@@ -0,0 +1,16 @@
+defmodule Advent2022.Day5 do
+  def input do
+    {:ok, data} = File.read("resources/day5.txt")
+    [stacks, instructions] = String.split(data, "\n\n")
+
+    [stacks, instructions]
+  end
+
+  def parse_stacks(input) do
+    input
+  end
+
+  def part1 do
+
+  end
+end
